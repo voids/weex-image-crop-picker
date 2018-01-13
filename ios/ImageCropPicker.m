76831f98a7a1f2f3cccdc6e86014f2859215894c
voids <qiudong26@gmail.com>
2018-01-13 12:02:12 +0800

    Add support for all types of smart albums for ios (selfies, time lapses, etc.)

--- ios/ImageCropPicker.m
+++ ios/ImageCropPicker.m
@@ -222,11 +222,25 @@ WX_EXPORT_METHOD(@selector(cleanSingle:callback:))
             imagePickerController.showsNumberOfSelectedAssets = [[self.options objectForKey:@"showsSelectedCount"] boolValue];
             if ([self.options objectForKey:@"smartAlbums"] != nil) {
                 NSDictionary *smartAlbums = @{
-                                              @"UserLibrary" : @(PHAssetCollectionSubtypeSmartAlbumUserLibrary),
+                                              //cloud albums
                                               @"PhotoStream" : @(PHAssetCollectionSubtypeAlbumMyPhotoStream),
+                                              //smart albums
+                                              @"Generic" : @(PHAssetCollectionSubtypeSmartAlbumGeneric),
                                               @"Panoramas" : @(PHAssetCollectionSubtypeSmartAlbumPanoramas),
                                               @"Videos" : @(PHAssetCollectionSubtypeSmartAlbumVideos),
+                                              @"Favorites" : @(PHAssetCollectionSubtypeSmartAlbumFavorites),
+                                              @"Timepalses" : @(PHAssetCollectionSubtypeSmartAlbumTimelapses),
+                                              @"AllHidden" : @(PHAssetCollectionSubtypeSmartAlbumAllHidden),
+                                              @"RecentlyAdded" : @(PHAssetCollectionSubtypeSmartAlbumRecentlyAdded),
                                               @"Bursts" : @(PHAssetCollectionSubtypeSmartAlbumBursts),
+                                              @"SlomoVideos" : @(PHAssetCollectionSubtypeSmartAlbumSlomoVideos),
+                                              @"UserLibrary" : @(PHAssetCollectionSubtypeSmartAlbumUserLibrary),
+                                              @"SelfPortraits" : @(PHAssetCollectionSubtypeSmartAlbumSelfPortraits),
+                                              @"Screenshots" : @(PHAssetCollectionSubtypeSmartAlbumScreenshots),
+                                              @"DepthEffect" : @(PHAssetCollectionSubtypeSmartAlbumDepthEffect),
+                                              @"LivePhotos" : @(PHAssetCollectionSubtypeSmartAlbumLivePhotos),
+                                              @"Animated" : @(PHAssetCollectionSubtypeSmartAlbumAnimated),
+                                              @"LongExposure" : @(PHAssetCollectionSubtypeSmartAlbumLongExposures),
                                               };
                 NSMutableArray *albumsToShow = [NSMutableArray arrayWithCapacity:5];
                 for (NSString* album in [self.options objectForKey:@"smartAlbums"]) {
